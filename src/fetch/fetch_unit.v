ec034358323cc9ddc91e92a8c2077b3a864a212d
infinitin007 <nitin.agiwal05@gmail.com>
2024-11-08 03:55:35 +0530

    Update fetch_unit.v

--- src/fetch/fetch_unit.v
+++ src/fetch/fetch_unit.v
@@ -1,9 +1,8 @@
 module fetch_unit (
     input wire clk,
     input wire reset,
-    input wire stall,                        
-    input wire flush,                         
-    input wire branch_taken,                  // Signal that branch is taken
+    input wire stall,                                                 
+    input wire is_branch_taken,                  // Signal that branch is taken
     input wire [31:0] branch_target,          // Branch target address
     output reg [31:0] instr1,                 // First instruction for decode
     output reg [31:0] instr2                  // Second instruction for decode
@@ -15,23 +14,29 @@ module fetch_unit (
 
     // Initialize instruction memory (for simulation)
     initial begin
-        $readmemh("instructions.hex", instruction_memory);
+        $readmemh("../instructions.hex", instruction_memory);
     end
-
+    reg flush = 0;
     // PC update logic with branch handling and buffer control
     always @(posedge clk or posedge reset) begin
         if (reset) begin
             pc <= 0;
-        end else if (branch_taken) begin
+        end else if (is_branch_taken) begin
             pc <= branch_target;               // On branch, set PC to branch target
+            buffer[0] <= 0;                     // instead of 0 place the nop instruction
+            buffer[1] <= 0;                     // instead of 0 place the nop instruction
+            flush <= 1;
+        end else if (flush) begin
+            buffer[0] <= 0;                     // instead of 0 place the nop instruction
+            buffer[1] <= 0;                     // instead of 0 place the nop instruction
+            flush <= 0;
         end else if (!stall) begin
-                buffer[0] <= instruction_memory[pc[31:2]]; // Fetch to buffer[0]
-                pc <= pc + 4;
-                buffer[1] <= instruction_memory[pc[31:2]]; // Fetch to buffer[1]
+            buffer[0] <= instruction_memory[pc[31:2]]; // Fetch to buffer[0]
+            pc <= pc + 4;
+            buffer[1] <= instruction_memory[pc[31:2]]; // Fetch to buffer[1]
+            pc <= pc + 4;
         end
-
     end
-
     // Output instructions from buffer to decode stage
     always @(*) begin
         instr1 = buffer[0];

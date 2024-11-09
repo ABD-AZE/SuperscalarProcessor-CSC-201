b215bc5b7ef80f8fc44a83bb54240583711db841
Rohan Gupta <149098723+RohanGupta-7@users.noreply.github.com>
2024-11-10 04:00:56 +0530

    Update relayer.v

--- src/fetch/relayer.v
+++ src/fetch/relayer.v
@@ -3,7 +3,8 @@ module relayer_unit(
     input wire [15:0]instr2_in,
     output wire [15:0]instr1_o,
     output wire [15:0]instr2_o,
-    output wire issingleinstr
+    output wire issingleinstr,
+    output wire isstall
 );
     parameter nop = 16'h0;
     reg [15:0] buffer1 [0:2];    
@@ -19,13 +20,18 @@ module relayer_unit(
         buffer2[1] = 16'b0;
         buffer2[2] = 16'b0;
         singinstr = 16'b0;
+        issingleinstr_reg = 0;
+        isstall_reg = 0;
     end
     reg [15:0] instr1;
     reg [15:0] instr2;
+    reg isstall_reg;
     reg [15:0] instr1_out;
     reg [15:0] instr2_out;
     reg issingleinstr_reg;
     always @(*) begin
+        instr1 = instr1_in;
+        instr2 = instr2_in;
         if (singinstr[15:12] != 4'b0) begin
             instr2 = instr1;
             instr1 = singinstr;
@@ -49,6 +55,7 @@ module relayer_unit(
         if(p==1) begin
             instr1_out = nop;
             instr2_out = nop;
+            isstall_reg = 1;
         end
         else begin
             instr1_out = instr1;
@@ -63,28 +70,33 @@ module relayer_unit(
             else begin
                 p=0;
             end
-        end
-        for(i=0;i<3;i=i+1) begin
-            if(buffer2[i][15:12]!=nop) begin
-                if(!((buffer2[i][10:8]!=instr2[7:5]&&instr2[11]==1)||(buffer2[i][10:8]!=instr2[7:5]&&buffer2[i][10:8]!=instr2[4:2]&&instr2[11]==0))) begin
-                    p=1;
-                end
-            end
-        end
-        if(p==0) begin
-            for(i=0;i<3;i=i+1) begin
-                if(buffer1[i][15:12]!=nop) begin
-                    if(!((buffer1[i][10:8]!=instr2[7:5]&&instr2[11]==1)||(buffer1[i][10:8]!=instr2[7:5]&&buffer1[i][10:8]!=instr2[4:2]&&instr2[11]==0))) begin
-                        p=1;
+            if(p==0) begin
+                for(i=0;i<3;i=i+1) begin
+                    if(buffer2[i][15:12]!=nop) begin
+                        if(!((buffer2[i][10:8]!=instr2[7:5]&&instr2[11]==1)||(buffer2[i][10:8]!=instr2[7:5]&&buffer2[i][10:8]!=instr2[4:2]&&instr2[11]==0))) begin
+                            p=1;
+                        end
                     end
                 end
+                if(p==0) begin
+                    for(i=0;i<3;i=i+1) begin
+                        if(buffer1[i][15:12]!=nop) begin
+                            if(!((buffer1[i][10:8]!=instr2[7:5]&&instr2[11]==1)||(buffer1[i][10:8]!=instr2[7:5]&&buffer1[i][10:8]!=instr2[4:2]&&instr2[11]==0))) begin
+                                p=1;
+                            end
+                        end
+                    end
+                end
+                if (p==0) begin
+                    instr2_out = instr2;
+                end
+                else begin
+                    instr2_out = nop;
+                end
             end
-        end
-        if (p==0) begin
-            instr2_out = instr2;
-        end
-        else begin
-            instr2_out = nop;
+            else begin
+                instr2_out = nop;
+            end
         end
         for(i=1;i<3;i++) begin
             buffer1[i] = buffer1[i-1];
@@ -103,10 +115,9 @@ module relayer_unit(
         else begin
             issingleinstr_reg = 0;
         end
-        instr1 = instr1_in;
-        instr2 = instr2_in;
     end
     assign instr1_o = instr1_out;
     assign instr2_o = instr2_out;
     assign issingleinstr = issingleinstr_reg;
+    assign isstall = isstall_reg;
 endmodule
